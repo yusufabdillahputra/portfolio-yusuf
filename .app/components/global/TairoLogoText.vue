9576d2e03d73d0f62589c2c7a4f1a2bc8ae30b06
Yusuf Abdillah Putra <yusufabdillahputra@gmail.com>
2025-01-26 12:37:07 +0700

    Update Flex on Tairo Logo Text

--- .app/components/global/TairoLogoText.vue
+++ .app/components/global/TairoLogoText.vue
@@ -1,10 +1,14 @@
 <template>
-  <BaseHeading
-    as="h2"
-    size="md"
-    weight="bold"
-    class="text-muted-800 dark:text-white"
-  >
-    Yusuf Abdillah Putra
-  </BaseHeading>
+  <div class="flex w-full items-center justify-center">
+    <BaseHeading
+      as="div"
+      size="md"
+      weight="bold"
+      class="text-muted-800 shrink-0 dark:text-white"
+    >
+      Yusuf Abdillah Putra
+    </BaseHeading>
+  </div>
 </template>
+<script setup lang="ts">
+</script>
